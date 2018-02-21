1abf3d00327008a1769881f8956aaca88bf03366
Shalaka Aigal <shalaka.aigal@gmail.com>
2018-02-20 17:18:55 -0800

    Webserver_Project

---
+++ src/conf/Htaccess.java
@@ -0,0 +1,102 @@
+package conf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import server.Constants;
+
+/**
+ * 
+ * @author shalaka
+ *
+ */
+public class Htaccess {
+	
+	File htaccessFile = null;
+	Scanner scanner = null;
+	
+	private String userFile;
+	private String authType;
+	private String authName;
+	private String require;
+	
+	HttpdConf configuration;
+	
+	public Htaccess(HttpdConf config) {
+		configuration = config;
+		htaccessFile = new File(Constants.HTACCESS_LOCATION);
+	}
+	
+	public void load() {
+		
+	
+		try {
+	         scanner = new Scanner(htaccessFile);
+	         scanner.useDelimiter("\\r?\\n");
+	        while(scanner.hasNextLine()) 
+		        {
+		        	String content = scanner.nextLine();
+		        	content.trim();
+		        	String[] config = content.split("\\s");
+		        	String key = config[0].trim();
+		        	String value = null;
+		        	if(config[1].startsWith("\"")){
+		        		config[1] = config[1].replaceAll("\"", "");	
+		        	}
+		        	value = config[1];
+		        	populateHtaccess(key, value);
+		        }
+			}
+		catch(FileNotFoundException ex) {
+			System.out.println("Htaccess file not found "+ex);
+		}
+		finally {
+	        if (scanner != null) {
+	            scanner.close();
+	        }
+		
+		}
+	}
+	
+	public boolean isAuthorized(String username, String password) {
+		return false;
+	}
+
+	private void populateHtaccess(String key, String value) {
+		switch (key) {
+		case Constants.AUTH_USER_FILE:
+			userFile = value;
+			break;
+		case Constants.AUTH_TYPE:
+			authType = value;
+			break;
+		case Constants.AUTH_NAME:
+			authName = value;
+			break;
+		case Constants.REQUIRE:
+			require = value;
+			break;
+		default:
+			System.out.println("Invalid Selection");
+			break;
+		}
+	}
+
+	public String getUserFile() {
+		return userFile;
+	}
+
+	public String getAuthType() {
+		return authType;
+	}
+
+	public String getAuthName() {
+		return authName;
+	}
+
+	public String getRequire() {
+		return require;
+	}
+
+}
---
+++ src/conf/HttpdConf.java
@@ -0,0 +1,127 @@
+package conf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import server.Constants;
+
+/**
+ * 
+ * @author shalaka
+ *
+ */
+public class HttpdConf {
+	
+	File httpdFile = null;
+
+	private String serverRoot = null;
+	private String documentRoot = null;
+	private int listen = 8080;
+	private String logFile = null;
+	private HashMap<String, String> alias = new HashMap<String, String>();
+	private HashMap<String, String> scriptAlias = new HashMap<String, String>();
+	private String accessFileName = null;
+	private String directoryIndex = null;
+	
+	public HttpdConf(String fileName) {
+		httpdFile = new File(fileName);
+	}
+	
+	public void load() {
+		Scanner scanner = null;
+
+		try {
+			scanner = new Scanner(httpdFile);
+	         scanner.useDelimiter("\\r?\\n");
+	        while(scanner.hasNextLine()) {
+	        	String content = scanner.nextLine();
+	        	content.trim();
+	        	String[] config = content.split("\\s");
+	        	String key = config[0].trim();
+	        	String value = config[1];
+	        	value = value.replaceAll("\"", "").trim();
+	        	String value1 = null;
+	        	if(config.length > 2) {
+	        		 value1 = config[2];
+		        		value1 = value1.replaceAll("\"", "").trim();
+	        	}
+	        	populateHttpdConf(key, value, value1);
+	        }
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("HttpdConf file not found "+ex);
+		}
+		finally{
+			if (scanner != null) {
+	            scanner.close();
+	        }
+		}
+	}
+
+	private void populateHttpdConf(String key, String value, String value1) {
+		switch (key) {
+		case Constants.SERVER_ROOT:
+			serverRoot = value;
+			break;
+		case Constants.DOCUMENT_ROOT:
+			documentRoot = value;
+			break;
+		case Constants.LISTEN:
+			listen = Integer.parseInt(value);
+			break;
+		case Constants.LOG_FILE:
+			logFile = value;
+			break;
+		case Constants.SCRIPT_ALIAS:
+			scriptAlias.put(value, value1);
+			break;
+		case Constants.ALIAS:
+			alias.put(value, value1);
+			break;
+		case Constants.ACCESS_FILE_NAME:
+			accessFileName = value;
+			break;
+		case Constants.DIRECTORY_INDEX:
+			directoryIndex = value;
+			break;
+		default:
+			System.out.println("Invalid Selection");
+			break;
+		}
+	}
+
+	public String getServerRoot() {
+		return serverRoot;
+	}
+
+	public String getDocumentRoot() {
+		return documentRoot;
+	}
+
+	public int getListen() {
+		return listen;
+	}
+
+	public String getLogFile() {
+		return logFile;
+	}
+
+	public HashMap<String, String> getAlias() {
+		return alias;
+	}
+
+	public HashMap<String, String> getScriptAlias() {
+		return scriptAlias;
+	}
+
+	public String getAccessFileName() {
+		return accessFileName;
+	}
+
+	public String getDirectoryIndex() {
+		return directoryIndex;
+	}
+
+}
